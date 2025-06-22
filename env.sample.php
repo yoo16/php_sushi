73ac80cfeac66fdc7f0a27741e92992b31a17749
yse-yoo <youhei_yoshikawa@yse-c.net>
2025-06-23 08:59:34 +0900

    update

--- env.sample.php
+++ env.sample.php
@@ -1,19 +1,18 @@
 <?php
 // Database
 const DB_CONNECTION = 'mysql';
-const DB_HOST = '127.0.0.1';
+const DB_HOST = 'mysql';
 const DB_PORT = 3306;
 const DB_DATABASE = 'sushi';
 const DB_USERNAME = 'root';
-// XAMP
-const DB_PASSWORD = '';
-// MAMP or Other
-// const DB_PASSWORD = 'root';
+const DB_PASSWORD = 'root';
 
-// アプリキー
+// アプリキー（セッションキー兼用）
 const APP_KEY = "haru_sushi";
 
 // サイトタイトル
 const SITE_TITLE = "はる寿司";
 
+// 個別で設定する場合
 // const BASE_URL = "http://localhost/php_sns/";
+// const BASE_URL = "http://localhost:8888/projects/php_mysql/php_sns/";
